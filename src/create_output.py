0e2295fe7a574c1e6f209ad1e2ad2d2b10650b79
Lucas Jenkins <lucas.jenkins.232@gmail.com>
2018-12-11 23:21:34 -0800

    Add files from lucas branch that should be there, remove files that shouldn't.

---
+++ src/create_output.py
@@ -0,0 +1,23 @@
+import numpy as np
+import os
+def format_output(y_pred, round=False):
+    """
+    Takes as input a np.ndarray of predictions, which may be real numbers, and rounds them to the nearest integer between 1 and 5
+    """
+    length = y_pred.shape[0]
+    fives = np.full(length, 5)
+    ones = np.full(length, 1)
+    if round:
+        y_pred = np.rint(y_pred)
+    return np.maximum(ones, np.minimum(fives, y_pred))
+
+def write_output(y_pred, fname):
+    fname = 'output/' + fname
+    if os.getcwd()[-3:] == 'src':
+        fname = '../' + fname
+    with open(f'{fname}.csv', 'w') as f:
+        f.write("index,stars\n")
+        i = 0
+        for val in y_pred:
+            f.write(f'{i},{val}\n')
+            i += 1
